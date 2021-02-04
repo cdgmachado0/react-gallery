89d0362f40cccb8ab1959b8a59af8ef77135674a
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-02-04 23:27:05 +0100

    All components added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 //Import components
 import SearchForm from './components/SearchForm';
+import Nav from './components/Nav';
+import PhotosList from './components/PhotosList';
 
 const App = () => {
   return (
-    <SearchForm />
+    <div className='container'>
+      <SearchForm />
+      <Nav />
+      <div className='photo-container'>
+        <PhotosList />
+      </div>
+    </div>
   );
 }
 
