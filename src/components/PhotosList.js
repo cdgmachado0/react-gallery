0d54777861c8155e4b9757a546353fdf4d0e0e29
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-02-09 20:04:43 +0100

    Got to work when pasting the URL, but I stil have to try it out with the other components to know I didn't brake anythig

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Photo from './Photo';
 
 
-const PhotosList = (props) => {
-    let photos = [];
-    if (props.images) {
-        photos = props.images
-            .map(photo => 
-                <Photo 
-                    source={photo.url} 
-                    key={photo.id}
-                />)
+class PhotosList extends Component {
+    
+
+    // if (props.images) {
+    //     photos = props.images
+    //         .map(photo => 
+    //             <Photo 
+    //                 source={photo.url} 
+    //                 key={photo.id}
+    //             />)
+    // }
+
+    state = {
+        photos: []
+    };
+
+
+    componentDidMount() {
+        let photos = [];
+        this.props.get(this.props.search)
+            .then(() => {
+                photos = this.props.images.map(photo => 
+                    <Photo 
+                        source={photo.url} 
+                        key={photo.id}
+                    />);
+                this.setState({ photos });
+            });
     }
 
+    render() {
+        // let photos = [];
+    // const { search } = this.props.match.match.params;
+    // console.log(this.props.match);
+
+    
     return (
         <React.Fragment>
             <h2>Results</h2>
             <ul> 
-                { photos }
+                { this.state.photos }
             </ul>
         </React.Fragment>
     );
+    }
 }
 
 
