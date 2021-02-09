0a313dd87dd4a590042498c69435d16472c04fa4
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-02-09 20:12:48 +0100

    Done with Excees except the Loading indicator

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 
-const Nav = (props) => (
+const Nav = () => (
     <nav className="main-nav">
         <ul>
-            <li onClick={ (e) => props.get(e.target.textContent.toLowerCase())}><NavLink to='/cats'>Cats</NavLink></li>
-            <li onClick={ (e) => props.get(e.target.textContent.toLowerCase())}><NavLink to='/dogs'>Dogs</NavLink></li>
-            <li onClick={ (e) => props.get(e.target.textContent.toLowerCase())}><NavLink to='/sports'>Sports</NavLink></li>
+            <li><NavLink to='/cats'>Cats</NavLink></li>
+            <li><NavLink to='/dogs'>Dogs</NavLink></li>
+            <li><NavLink to='/sports'>Sports</NavLink></li>
         </ul>
     </nav>
 )
