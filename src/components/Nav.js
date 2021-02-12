0e1c28ba2005609e0a2bdfc46517620389866e73
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-02-12 23:36:42 +0100

    3 out of 3. Still missing the history routing feature

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,26 +5,15 @@ import Photo from './Photo';
 
 class Nav extends Component {
 
-    handleSearch = (query) => {
-        this.props.get(query)
-            .then(data => {
-                let images = data.map(photo => 
-                    <Photo 
-                        source={photo.url} 
-                        key={photo.id}
-                    />);
-                this.props.set(images);
-                this.props.history.push(`/${query}`);
-            });
-      } 
+    
 
     render() {
         return (
             <nav className="main-nav">
                 <ul>
-                    <li onClick={ (e) => this.handleSearch(e.target.textContent.toLowerCase()) } ><NavLink to='/cats'>Cats</NavLink></li>
-                    <li><NavLink to='/dogs'>Dogs</NavLink></li>
-                    <li><NavLink to='/sports'>Sports</NavLink></li>
+                    <li onClick={ (e) => this.props.get(e.target.textContent.toLowerCase()) } ><NavLink to='/cats'>Cats</NavLink></li>
+                    <li onClick={ (e) => this.props.get(e.target.textContent.toLowerCase()) }><NavLink to='/dogs'>Dogs</NavLink></li>
+                    <li onClick={ (e) => this.props.get(e.target.textContent.toLowerCase()) }><NavLink to='/sports'>Sports</NavLink></li>
                 </ul>
             </nav>
         );
